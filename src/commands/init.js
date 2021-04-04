7eb6999706c89f9f14c8f00c78724af0c7fbf583
henriquegranatto` <henrique.ramires.granatto@gmail.com`>
2021-04-04 13:29:03 -0300

    .

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,5 +1,5 @@
 const command = {
-  name: 'service-cli',
+  name: 'init',
   run: async toolbox => {
     toolbox.print.info(await toolbox.system.run('npm install https://github.com/HenriqueGranatto/service-cli-module.git', { trim: true }))
   }
