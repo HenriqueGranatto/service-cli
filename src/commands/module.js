e08a5fea7e0b1bdd62fc49ab53ccc96340d5010b
henriquegranatto` <henrique.ramires.granatto@gmail.com`>
2021-04-04 14:16:49 -0300

    .

--- src/commands/module.js
+++ src/commands/module.js
@@ -1,7 +1,9 @@
 const command = {
   name: 'module',
   run: async toolbox => {
+    toolbox.print.info("Adicionando gerenciador de módulos")
     toolbox.print.info(await toolbox.system.run('npm install https://github.com/HenriqueGranatto/service-cli-module.git', { trim: true }))
+    toolbox.print.success("Gerenciador de módulos instalado")
   }
 }
 
